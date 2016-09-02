8bc3880ed99d6431c5360c859b11e3c36ca77b40
Loosper <boian4o1@gmail.com>
2016-09-03 00:43:22 +0300

    Completed the menu.

--- script.js
+++ script.js
@@ -1,107 +1,121 @@
-var menu = {
-    'Transfer': 0,
-    'Couple': 0,
-    'Enter': 2,
-    'Edit': 3,
-    'Reference': 4,
-}
-var mapping = ['Device', 'SIM', 'Couple', 'Transfer'];
-// menu = {'Enter': '', 'Couple': '', 'Edit': '', 'Reference': '', 'Transfer': ''};
-var subelements = {'Device': '', 'SIM': '', 'Couple': '', 'Transfer': ''};
-var mode = '';
-var body = '';
-
-// function load_enter(){
-//     if(mode[1] == 'SIM'){
-//         body.innerHTML = 'SIM';
-//     }else{
-//         body.innerHTML = 'Device';
-//     }
-//     // placeholder. illustrates what will kind of happen
-// }
-
-// function load_couple(){
-//     body.innerHTML = 'Couple';
-// }
-
-// function load_edit(){
-//     // ...create stuff in common
-//     if(mode[1] == 'Device'){
-//         body.innerHTML = 'Edit device';
-//     }else if(mode[1] == 'SIM'){
-//         body.innerHTML = 'Edit SIM';
-//     }else{
-//         body.innerHTML = 'Edit a couple';
-//     }
-// }
-
-// function load_reference(){
-//     // ...create stuff in common
-//     if(mode[1] == 'Device'){
-//         body.innerHTML = 'Query for device';
-//     }else if(mode[1] == 'SIM'){
-//         body.innerHTML = 'Query for SIM';
-//     }else{
-//         body.innerHTML = 'Wuery for a couple';
-//     }   
-// }
-
-// function load_transfer(){
-//     body.innerHTML = 'Transfer';
-// }
-
-function load_body(){
-    mode = mode.split('_');
-
-    
-}
-
-function set_submenu(){
-    var nav_bar = document.getElementById('secondary_bar');
-    // nav_bar.innerHTML = '';
-    while(nav_bar.firstChild){
-        nav_bar.removeChild(nav_bar.firstChild);
-    }
-
-    for(var i = 0; i < menu[mode]; i++){
-        nav_bar.appendChild(subelements[mapping[i]]);
-    }
-    load_body();
-}
-
-function main(){
-    body = document.getElementById('content');
-
-    for(var key in menu){
-        var element = document.getElementById(key);
-        element.onclick = function(i) {
-            return function(event) {
-                // console.log(event);
-                mode = i;
-                set_submenu();
-            }
-            // make Mr. document listen for these things and only parse the [event] object.
-            // Simplifies a lot
-        }(key);
-    }
-
-    for(key in subelements){
-        var link = document.createElement('a');
-        var div = document.createElement('div');
-        link.setAttribute('href', 'javascript:;');
-        div.className = 'nav_btn secondary_btn';
-        link.appendChild(div);
-        div.textContent = key;
-        div.onclick = function(){
-            mode += '_' + key;
-            load_body();
-            document.getElementById('secondary_bar').innerHTML = '';
-        };
-
-        if (key == 'Device') {
-            div.id = 'offset_btn';
+const mapping = {
+    'Enter': {
+        'Device': function (content) {
+            content.innerHTML = 'Enter Device';
+        },
+        'SIM': function (content) {
+            content.innerHTML = 'Enter SIM';
         }
-        subelements[key] = link;
+    },
+    'Couple': function (content) {
+        content.innerHTML = 'Couple';
+    },
+    'Edit': {
+        'Device': function (content) {
+            content.innerHTML = 'Edit Device';
+        },
+        'SIM': function (content) {
+            content.innerHTML = 'Edit SIM';
+        },
+        'Couple': function (content) {
+            content.innerHTML = 'Edit Couple';
+        }
+    },
+    'Reference': {
+        'Device': function (content) {
+            content.innerHTML = 'Reference Device';
+        },
+        'SIM': function (content) {
+            content.innerHTML = 'Reference SIM';
+        },
+        'Couple': function (content) {
+            content.innerHTML = 'Reference Couple';
+        },
+        'Transfer': function (content) {
+            content.innerHTML = 'Reference Transfer';
+        }
+    },
+    'Transfer': function (content ){
+        content.innerHTML = 'Transfer';
+    }
+};
+
+const menu_order = ['Enter', 'Couple', 'Edit', 'Reference', 'Transfer'];
+const submenu_order = ['Device', 'SIM', 'Couple', 'Transfer'];
+
+function  build_button (button, callback, submenu = true) {
+    var div = document.createElement('div');
+    div.className = 'nav_btn' +
+        (submenu ? ' secondary_btn': ' primary_btn') +
+        (button == 'Device' ? ' offset_btn': '');
+    div.textContent = button;
+    div.id = button;
+    div.onclick = callback;
+    return div;
+}
+
+function remove_children (element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+// ITS TWICE
+// PLEASE MAKE IT ONE
+function main () {
+    let elements = {};
+
+    function building_submenu_callback () {
+        let elements = {};
+        let menu = document.getElementById('submenu');
+        menu.innerHTML = '';
+
+        for (let button_type in mapping[this.id]) {
+            elements[button_type] = build_button(
+                button_type,
+                function (button, parent) {
+                    return function () {
+                        remove_children(document.getElementById('submenu'));
+                        mapping[parent][button](document.getElementById('content'));
+                    }
+                }(button_type, this.id),
+                true
+            );
+        }
+
+        for (let element of submenu_order) {
+            try {
+                // other typerrors that may arise are silenced. Make better checks
+                menu.appendChild(elements[element]);
+            } catch (exception) {
+                if (exception.name !== 'TypeError') throw exception;
+                // else console.log('Move on. This one isn\'t needed 2');
+            }
+        }
+    }
+
+    for (let button_type in mapping) {
+        // console.log(mapping[button_type]);
+        elements[button_type] = build_button(
+            button_type,
+            (typeof mapping[button_type] === 'function') ?
+            function (){
+                remove_children(document.getElementById('submenu'));
+                mapping[button_type](document.getElementById('content'));
+            } :
+            building_submenu_callback,
+            false
+        );
+    }
+
+    let menu = document.getElementById('menu');
+    for (let element of menu_order) {
+        try {
+            menu.appendChild(elements[element]);
+        } catch (exception) {
+            if (exception.name !== 'TypeError') throw exception;
+            // else console.log('Move on. This one isn\'t needed');
+        }
     }
 }
 
